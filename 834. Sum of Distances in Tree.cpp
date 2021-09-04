9231a96742bdebefd58e601d41d3279176061fb6
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2021-09-04 19:51:19 +0530

    Create 834. Sum of Distances in Tree.cpp
    There is an undirected connected tree with n nodes labeled from 0 to n - 1 and n - 1 edges.
    
    You are given the integer n and the array edges where edges[i] = [ai, bi] indicates that there is an edge between nodes ai and bi in the tree.
    
    Return an array answer of length n where answer[i] is the sum of the distances between the ith node in the tree and all other nodes.
    
     
    
    Example 1:
    
    
    Input: n = 6, edges = [[0,1],[0,2],[2,3],[2,4],[2,5]]
    Output: [8,12,6,10,10,10]
    Explanation: The tree is shown above.
    We can see that dist(0,1) + dist(0,2) + dist(0,3) + dist(0,4) + dist(0,5)
    equals 1 + 1 + 2 + 2 + 2 = 8.
    Hence, answer[0] = 8, and so on.
    Example 2:
    
    
    Input: n = 1, edges = []
    Output: [0]
    Example 3:
    
    
    Input: n = 2, edges = [[1,0]]
    Output: [1,1]
     
    
    Constraints:
    
    1 <= n <= 3 * 104
    edges.length == n - 1
    edges[i].length == 2
    0 <= ai, bi < n
    ai != bi
    The given input represents a valid tree.
    
    Output Status:
    
    Runtime: 1339 ms, faster than 5.18% of C++ online submissions for Sum of Distances in Tree.
    Memory Usage: 153.5 MB, less than 5.78% of C++ online submissions for Sum of Distances in Tree.

---
+++ 834. Sum of Distances in Tree.cpp
@@ -0,0 +1,44 @@
+Output Status:
+
+Runtime: 1339 ms, faster than 5.18% of C++ online submissions for Sum of Distances in Tree.
+Memory Usage: 153.5 MB, less than 5.78% of C++ online submissions for Sum of Distances in Tree.
+  
+  class Solution {
+public:
+	vector<vector<int>> tree;
+	map<pair<int,int>,int> m;
+	map<pair<int,int>,int> under;
+
+	pair<int,int> getDfs(int i,int p) {
+
+		int ans=0,c=1;
+		for(int j: tree[i]) {
+			if(j==p) continue;
+			if(m.find({i,j})==m.end()) {
+				auto p= getDfs(j,i);
+				m[{i,j}]=p.first;
+				under[{i,j}]=p.second;
+			}
+			ans+= m[{i,j}]+under[{i,j}];
+			c+=under[{i,j}];
+		}
+		return {ans,c};
+	}
+
+	vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges) {
+		m.clear();
+		under.clear();
+
+		tree.resize(n);
+		vector<int> ans;
+		for(auto i: edges) {
+			tree[i[0]].push_back(i[1]);
+			tree[i[1]].push_back(i[0]);
+		}
+
+		for(int i=0;i<n;i++) {
+			ans.push_back(getDfs(i,-1).first);
+		}
+		return ans;
+	}
+}
