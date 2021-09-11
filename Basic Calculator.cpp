428292a2a32d277435dcf8d4baf3810a7bb2904c
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2021-09-11 17:40:34 +0530

    Create Basic Calculator.cpp
    Given a string s representing a valid expression, implement a basic calculator to evaluate it, and return the result of the evaluation.
    
    Note: You are not allowed to use any built-in function which evaluates strings as mathematical expressions, such as eval().
    
     
    
    Example 1:
    
    Input: s = "1 + 1"
    Output: 2
    Example 2:
    
    Input: s = " 2-1 + 2 "
    Output: 3
    Example 3:
    
    Input: s = "(1+(4+5+2)-3)+(6+8)"
    Output: 23
     
    
    Constraints:
    
    1 <= s.length <= 3 * 105
    s consists of digits, '+', '-', '(', ')', and ' '.
    s represents a valid expression.
    '+' is not used as a unary operation.
    '-' could be used as a unary operation but it has to be inside parentheses.
    There will be no two consecutive operators in the input.
    Every number and running calculation will fit in a signed 32-bit integer.
    
    Output Status:
    
    Runtime: 14 ms, faster than 39.80% of C++ online submissions for Basic Calculator.
    Memory Usage: 8.2 MB, less than 46.32% of C++ online submissions for Basic Calculator.

---
+++ Basic Calculator.cpp
@@ -0,0 +1,45 @@
+Output Status:
+
+Runtime: 14 ms, faster than 39.80% of C++ online submissions for Basic Calculator.
+Memory Usage: 8.2 MB, less than 46.32% of C++ online submissions for Basic Calculator.
+
+class Solution {
+public:
+    int calculate(string s) {
+        int ans = 0;
+        int n = s.size();
+        stack<int>st;
+        int sign = 1;
+        
+        for(int i=0; i<n; i++){
+            if(isdigit(s[i])){
+                int val = 0;
+                while(isdigit(s[i])){
+                    val = val*10 + (s[i]-'0');
+                    i++;
+                }       
+                i--;
+                val = sign*val;
+                ans = ans + val;
+                sign = 1;
+            }
+            else if(s[i] == '('){
+                st.push(ans);
+                st.push(sign);
+                ans = 0;
+                sign=1;
+            }
+            else if(s[i] == ')'){
+                ans = ans*st.top();
+                st.pop();
+                ans = ans + st.top();
+                st.pop();
+            }
+            else if(s[i] == '-'){
+                sign = -1;
+            }
+        }
+        return ans;
+    }
+};
+
