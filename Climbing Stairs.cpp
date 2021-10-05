7bdea6b5165874a15a5100306c70b78e0d6ef078
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2021-10-05 11:08:01 +0530

    Create Climbing Stairs.cpp
    You are climbing a staircase. It takes n steps to reach the top.
    
    Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
    
     
    
    Example 1:
    
    Input: n = 2
    Output: 2
    Explanation: There are two ways to climb to the top.
    1. 1 step + 1 step
    2. 2 steps
    Example 2:
    
    Input: n = 3
    Output: 3
    Explanation: There are three ways to climb to the top.
    1. 1 step + 1 step + 1 step
    2. 1 step + 2 steps
    3. 2 steps + 1 step
     
    
    Constraints:
    
    1 <= n <= 45
    
    Output Status:
    
    Runtime: 0 ms, faster than 100.00% of C++ online submissions for Climbing Stairs.
    Memory Usage: 6.3 MB, less than 14.24% of C++ online submissions for Climbing Stairs.

---
+++ Climbing Stairs.cpp
@@ -0,0 +1,22 @@
+Output Status:
+
+Runtime: 0 ms, faster than 100.00% of C++ online submissions for Climbing Stairs.
+Memory Usage: 6.3 MB, less than 14.24% of C++ online submissions for Climbing Stairs.
+
+
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        
+       vector<int> DP(n+1, -1);  
+        return climb(n, DP);
+    }
+    
+    int climb(int n, vector<int> &DP)
+    {
+        if(n == 0 || n == 1) return 1;                             
+        return DP[n] != -1?DP[n] : DP[n] = climb(n-1, DP) + climb(n-2, DP);
+        
+    }
+};
