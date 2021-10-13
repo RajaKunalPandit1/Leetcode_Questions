45a914ab154c8a3e72fced5fc25b948981748c19
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2021-10-13 11:05:20 +0530

    Create Triangle.cpp
    Given a triangle array, return the minimum path sum from top to bottom.
    
    For each step, you may move to an adjacent number of the row below. More formally, if you are on index i on the current row, you may move to either index i or index i + 1 on the next row.
    
     
    
    Example 1:
    
    Input: triangle = [[2],[3,4],[6,5,7],[4,1,8,3]]
    Output: 11
    Explanation: The triangle looks like:
       2
      3 4
     6 5 7
    4 1 8 3
    The minimum path sum from top to bottom is 2 + 3 + 5 + 1 = 11 (underlined above).
    Example 2:
    
    Input: triangle = [[-10]]
    Output: -10
     
    
    Constraints:
    
    1 <= triangle.length <= 200
    triangle[0].length == 1
    triangle[i].length == triangle[i - 1].length + 1
    -104 <= triangle[i][j] <= 104
    
    
    Output Status:
    
    Runtime: 11 ms, faster than 17.85% of C++ online submissions for Triangle.
    Memory Usage: 8.5 MB, less than 66.97% of C++ online submissions for Triangle.

---
+++ Triangle.cpp
@@ -0,0 +1,15 @@
+Output Status:
+
+Runtime: 11 ms, faster than 17.85% of C++ online submissions for Triangle.
+Memory Usage: 8.5 MB, less than 66.97% of C++ online submissions for Triangle.
+
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+         vector<int> mini = triangle[triangle.size()-1];
+        for ( int i = triangle.size() - 2; i>= 0 ; --i )
+            for ( int j = 0; j < triangle[i].size() ; ++ j )
+                mini[j] = triangle[i][j] + min(mini[j],mini[j+1]);
+        return mini[0];
+    }
+};
