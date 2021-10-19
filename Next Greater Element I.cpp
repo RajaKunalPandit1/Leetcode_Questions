839b1a1e58b5ed3d1e98c18599088395d7896119
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2021-10-19 14:46:52 +0530

    Create Next Greater Element I.cpp
    The next greater element of some element x in an array is the first greater element that is to the right of x in the same array.
    
    You are given two distinct 0-indexed integer arrays nums1 and nums2, where nums1 is a subset of nums2.
    
    For each 0 <= i < nums1.length, find the index j such that nums1[i] == nums2[j] and determine the next greater element of nums2[j] in nums2. If there is no next greater element, then the answer for this query is -1.
    
    Return an array ans of length nums1.length such that ans[i] is the next greater element as described above.
    
     
    
    Example 1:
    
    Input: nums1 = [4,1,2], nums2 = [1,3,4,2]
    Output: [-1,3,-1]
    Explanation: The next greater element for each value of nums1 is as follows:
    - 4 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
    - 1 is underlined in nums2 = [1,3,4,2]. The next greater element is 3.
    - 2 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
    Example 2:
    
    Input: nums1 = [2,4], nums2 = [1,2,3,4]
    Output: [3,-1]
    Explanation: The next greater element for each value of nums1 is as follows:
    - 2 is underlined in nums2 = [1,2,3,4]. The next greater element is 3.
    - 4 is underlined in nums2 = [1,2,3,4]. There is no next greater element, so the answer is -1.
     
    
    Constraints:
    
    1 <= nums1.length <= nums2.length <= 1000
    0 <= nums1[i], nums2[i] <= 104
    All integers in nums1 and nums2 are unique.
    All the integers of nums1 also appear in nums2.
    
    Output Status:
    
    Runtime: 12 ms, faster than 27.88% of C++ online submissions for Next Greater Element I.
    Memory Usage: 8.9 MB, less than 58.15% of C++ online submissions for Next Greater Element I.

---
+++ Next Greater Element I.cpp
@@ -0,0 +1,34 @@
+Output Status:
+
+Runtime: 12 ms, faster than 27.88% of C++ online submissions for Next Greater Element I.
+Memory Usage: 8.9 MB, less than 58.15% of C++ online submissions for Next Greater Element I.
+
+
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int>st;
+        unordered_map<int,int>mp; //we will be using map as nextgrt array
+        for(int i=nums2.size()-1;i>=0;i--)
+        {
+            while(!st.empty() && st.top()<=nums2[i])
+            {
+                st.pop();
+            }
+            if(st.empty())
+                mp[nums2[i]]=-1;
+            else
+                mp[nums2[i]]=st.top();
+        
+            st.push(nums2[i]);
+        }
+        //matching and putting value in output vector
+        vector<int>out;
+        for(int i=0;i<nums1.size();i++)
+        {
+            out.push_back(mp[nums1[i]]);
+        }
+        return out;
+        
+    }
+};
