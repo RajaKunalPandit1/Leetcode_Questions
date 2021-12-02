a00405ac964c9451eebfe9ad83bbe4c1b1ef98d2
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2021-12-02 15:04:58 +0530

    Create Odd Even Linked List.cpp
    Output Status:
    
    Runtime: 12 ms, faster than 62.79% of C++ online submissions for Odd Even Linked List.
    Memory Usage: 10.4 MB, less than 72.43% of C++ online submissions for Odd Even Linked List.
    
    Given the head of a singly linked list, group all the nodes with odd indices together followed by the nodes with even indices, and return the reordered list.
    
    The first node is considered odd, and the second node is even, and so on.
    
    Note that the relative order inside both the even and odd groups should remain as it was in the input.
    
    You must solve the problem in O(1) extra space complexity and O(n) time complexity.
    
     
    
    Example 1:
    
    
    Input: head = [1,2,3,4,5]
    Output: [1,3,5,2,4]
    Example 2:
    
    
    Input: head = [2,1,3,5,6,4,7]
    Output: [2,3,6,7,1,5,4]
     
    
    Constraints:
    
    n == number of nodes in the linked list
    0 <= n <= 104
    -106 <= Node.val <= 106

---
+++ Odd Even Linked List.cpp
@@ -0,0 +1,31 @@
+Output Status:
+
+Runtime: 12 ms, faster than 62.79% of C++ online submissions for Odd Even Linked List.
+Memory Usage: 10.4 MB, less than 72.43% of C++ online submissions for Odd Even Linked List.
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if (!head) return nullptr;
+        ListNode *odd = head, *even = head->next, *evenHead = even;
+        while (odd->next && even->next) {
+            odd->next = even->next;
+            odd = odd->next;
+            even->next = odd->next;
+            even = even->next;
+        }
+        odd->next = evenHead;
+        return head;
+    }
+};
