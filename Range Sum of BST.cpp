c4bfba09ef649af91922471d199501859597abda
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2021-12-14 16:33:30 +0530

    Create Range Sum of BST.cpp
    Output Status:
    
    Runtime: 164 ms, faster than 19.40% of C++ online submissions for Range Sum of BST.
    Memory Usage: 64.7 MB, less than 18.44% of C++ online submissions for Range Sum of BST.
    
    Given the root node of a binary search tree and two integers low and high, return the sum of values of all nodes with a value in the inclusive range [low, high].
    
     
    
    Example 1:
    
    
    Input: root = [10,5,15,3,7,null,18], low = 7, high = 15
    Output: 32
    Explanation: Nodes 7, 10, and 15 are in the range [7, 15]. 7 + 10 + 15 = 32.
    Example 2:
    
    
    Input: root = [10,5,15,3,7,13,18,1,null,6], low = 6, high = 10
    Output: 23
    Explanation: Nodes 6, 7, and 10 are in the range [6, 10]. 6 + 7 + 10 = 23.
     
    
    Constraints:
    
    The number of nodes in the tree is in the range [1, 2 * 104].
    1 <= Node.val <= 105
    1 <= low <= high <= 105
    All Node.val are unique.

---
+++ Range Sum of BST.cpp
@@ -0,0 +1,34 @@
+Output Status:
+
+Runtime: 164 ms, faster than 19.40% of C++ online submissions for Range Sum of BST.
+Memory Usage: 64.7 MB, less than 18.44% of C++ online submissions for Range Sum of BST.
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+     int sum=0;
+    int inorder(TreeNode* root,int L,int R)
+    {
+        if(root){
+        inorder(root->left,L,R);
+        if(root->val>=L && root->val<=R)
+            sum+=root->val;
+        inorder(root->right,L,R);
+        }
+        return sum;
+    }
+    int rangeSumBST(TreeNode* root, int L, int R) {
+        if(!root)return 0;
+        return inorder(root,L,R);
+    }
+};
