717a54249bac5c4948b70e16f171f33eae8413a7
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2021-12-24 22:21:01 +0530

    Create Merge Intervals.cpp
    Output Status: 
    
    Runtime: 16 ms, faster than 88.84% of C++ online submissions for Merge Intervals.
    Memory Usage: 14 MB, less than 94.91% of C++ online submissions for Merge Intervals.
    
    Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
    
     
    
    Example 1:
    
    Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
    Output: [[1,6],[8,10],[15,18]]
    Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
    Example 2:
    
    Input: intervals = [[1,4],[4,5]]
    Output: [[1,5]]
    Explanation: Intervals [1,4] and [4,5] are considered overlapping.
     
    
    Constraints:
    
    1 <= intervals.length <= 104
    intervals[i].length == 2
    0 <= starti <= endi <= 104

---
+++ Merge Intervals.cpp
@@ -0,0 +1,19 @@
+Output Status: 
+
+Runtime: 16 ms, faster than 88.84% of C++ online submissions for Merge Intervals.
+Memory Usage: 14 MB, less than 94.91% of C++ online submissions for Merge Intervals.
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+         if(intervals.size()<=1) return intervals;
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> output;
+        output.push_back(intervals[0]);
+        for(int i=1; i<intervals.size(); i++) {
+            if(output.back()[1] >= intervals[i][0]) output.back()[1] = max(output.back()[1] , intervals[i][1]);
+            else output.push_back(intervals[i]); 
+        }
+        return output;
+    }
+};
