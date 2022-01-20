7917d602393b3e7ef925dcdceaf5329bc67657a7
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2022-01-20 20:54:22 +0530

    Create Koko Eating Bananas.cpp
    Output Status: 
    
    Runtime: 90 ms, faster than 14.69% of C++ online submissions for Koko Eating Bananas.
    Memory Usage: 18.8 MB, less than 53.71% of C++ online submissions for Koko Eating Bananas.
    
    Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
    
    Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and eats k bananas from that pile. If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour.
    
    Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.
    
    Return the minimum integer k such that she can eat all the bananas within h hours.
    
     
    
    Example 1:
    
    Input: piles = [3,6,7,11], h = 8
    Output: 4
    Example 2:
    
    Input: piles = [30,11,23,4,20], h = 5
    Output: 30
    Example 3:
    
    Input: piles = [30,11,23,4,20], h = 6
    Output: 23
     
    
    Constraints:
    
    1 <= piles.length <= 104
    piles.length <= h <= 109
    1 <= piles[i] <= 109

---
+++ Koko Eating Bananas.cpp
@@ -0,0 +1,22 @@
+Output Status: 
+
+Runtime: 90 ms, faster than 14.69% of C++ online submissions for Koko Eating Bananas.
+Memory Usage: 18.8 MB, less than 53.71% of C++ online submissions for Koko Eating Bananas.
+
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int H) {
+         int low = 1, high = 1000000000, k = 0;
+        while (low <= high) {
+            k = (low + high) / 2;
+            int h = 0;
+            for (int i = 0; i < piles.size(); i ++) 
+                h += ceil(1.0 * piles[i] / k);
+            if (h > H)
+                low = k + 1;
+            else
+                high = k - 1;
+        }
+        return low;
+    }
+};
