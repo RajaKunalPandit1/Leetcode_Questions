bc3d0b7103a069c4f986cfbd8f411a03830cae92
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2022-02-05 20:42:33 +0530

    Create Merge k Sorted Lists.cpp
    Output Status: 
    
    Runtime: 441 ms, faster than 11.18% of C++ online submissions for Merge k Sorted Lists.
    Memory Usage: 13.4 MB, less than 58.51% of C++ online submissions for Merge k Sorted Lists.
    
    
    You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
    
    Merge all the linked-lists into one sorted linked-list and return it.
    
     
    
    Example 1:
    
    Input: lists = [[1,4,5],[1,3,4],[2,6]]
    Output: [1,1,2,3,4,4,5,6]
    Explanation: The linked-lists are:
    [
      1->4->5,
      1->3->4,
      2->6
    ]
    merging them into one sorted list:
    1->1->2->3->4->4->5->6
    Example 2:
    
    Input: lists = []
    Output: []
    Example 3:
    
    Input: lists = [[]]
    Output: []
     
    
    Constraints:
    
    k == lists.length
    0 <= k <= 10^4
    0 <= lists[i].length <= 500
    -10^4 <= lists[i][j] <= 10^4
    lists[i] is sorted in ascending order.
    The sum of lists[i].length won't exceed 10^4.

---
+++ Merge k Sorted Lists.cpp
@@ -0,0 +1,38 @@
+Output Status: 
+
+Runtime: 441 ms, faster than 11.18% of C++ online submissions for Merge k Sorted Lists.
+Memory Usage: 13.4 MB, less than 58.51% of C++ online submissions for Merge k Sorted Lists.
+
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* merge2Lists(ListNode* l1, ListNode* l2) {
+        if (!l1) return l2;
+        if (!l2) return l1;
+        ListNode* head = l1->val <= l2->val? l1 : l2;
+        head->next = l1->val <= l2->val ? merge2Lists(l1->next, l2) : merge2Lists(l1, l2->next);
+        return head;
+    }
+    
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if (lists.size() == 0) return NULL;
+        
+        ListNode* head = lists[0];
+        
+        for (int i = 1; i < lists.size(); i++)
+            head = merge2Lists(head, lists[i]);
+        
+        return head;
+    }
+};
