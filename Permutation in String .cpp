d6635450b863b870fd804f5713f8d6541e327de8
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2022-02-11 18:59:04 +0530

    Create Permutation in String .cpp
    Output Status: 
    
    Runtime: 404 ms, faster than 8.15% of C++ online submissions for Permutation in String.
    Memory Usage: 121.2 MB, less than 5.41% of C++ online submissions for Permutation in String.
    
    Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
    
    In other words, return true if one of s1's permutations is the substring of s2.
    
     
    
    Example 1:
    
    Input: s1 = "ab", s2 = "eidbaooo"
    Output: true
    Explanation: s2 contains one permutation of s1 ("ba").
    Example 2:
    
    Input: s1 = "ab", s2 = "eidboaoo"
    Output: false
     
    
    Constraints:
    
    1 <= s1.length, s2.length <= 104
    s1 and s2 consist of lowercase English letters.

---
+++ Permutation in String .cpp
@@ -0,0 +1,29 @@
+Output Status: 
+
+Runtime: 404 ms, faster than 8.15% of C++ online submissions for Permutation in String.
+Memory Usage: 121.2 MB, less than 5.41% of C++ online submissions for Permutation in String.
+
+
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+     int count1[26] = {0};
+        for(int i = 0; i < s1.size(); i++) count1[s1[i] - 'a']++;
+        
+        for(int i = 0; i < s2.size(); i++){
+            bool flag = true;
+            string str = s2.substr(i, s1.size());
+
+            int count2[26] = {0};
+            for(int j = 0; j < str.size(); j++) count2[str[j] - 'a']++;
+            
+            for(int idx = 0; idx < 26; idx++)
+                if(count1[idx] != count2[idx]){
+                    flag = false;
+                    break;
+                }
+            if(flag) return true;
+        }
+        return false;
+    }
+};
