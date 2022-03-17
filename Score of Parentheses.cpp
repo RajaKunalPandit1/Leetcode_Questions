df88a4a0b1d3db882ca57ba4786982230932b4f6
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2022-03-17 22:14:33 +0530

    Create Score of Parentheses.cpp
    Output Status: 
    
    Runtime: 0 ms, faster than 100.00% of C++ online submissions for Score of Parentheses.
    Memory Usage: 6.2 MB, less than 45.05% of C++ online submissions for Score of Parentheses.
    
    Given a balanced parentheses string s, return the score of the string.
    
    The score of a balanced parentheses string is based on the following rule:
    
    "()" has score 1.
    AB has score A + B, where A and B are balanced parentheses strings.
    (A) has score 2 * A, where A is a balanced parentheses string.
     
    
    Example 1:
    
    Input: s = "()"
    Output: 1
    Example 2:
    
    Input: s = "(())"
    Output: 2
    Example 3:
    
    Input: s = "()()"
    Output: 2
     
    
    Constraints:
    
    2 <= s.length <= 50
    s consists of only '(' and ')'.
    s is a balanced parentheses string.

---
+++ Score of Parentheses.cpp
@@ -0,0 +1,31 @@
+Output Status: 
+
+Runtime: 0 ms, faster than 100.00% of C++ online submissions for Score of Parentheses.
+Memory Usage: 6.2 MB, less than 45.05% of C++ online submissions for Score of Parentheses.
+
+class Solution {
+public:
+    int scoreOfParentheses(string s) {
+         stack<int> st;
+        int ind = 0;
+        
+        for(auto i : s)
+        {
+			// if we find open parenthesis
+			// push the current score into the stack
+            if(i == '(')
+            {
+                st.push(ind);
+				// reset the score to 0
+                ind = 0;
+            }
+            else // if we find close parenthesis
+            {
+                ind = st.top() + max(ind*2 ,1);
+                st.pop();
+            }
+                
+        }
+        return ind;
+    }
+};
