86051c30a917c84e44186b176ac7677507645769
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2022-04-07 10:07:42 +0530

    Create 1046. Last Stone Weight.cpp
    Output Status: 
    
    Runtime: 3 ms, faster than 55.52% of C++ online submissions for Last Stone Weight.
    Memory Usage: 7.6 MB, less than 34.27% of C++ online submissions for Last Stone Weight.
    
    You are given an array of integers stones where stones[i] is the weight of the ith stone.
    
    We are playing a game with the stones. On each turn, we choose the heaviest two stones and smash them together. Suppose the heaviest two stones have weights x and y with x <= y. The result of this smash is:
    
    If x == y, both stones are destroyed, and
    If x != y, the stone of weight x is destroyed, and the stone of weight y has new weight y - x.
    At the end of the game, there is at most one stone left.
    
    Return the smallest possible weight of the left stone. If there are no stones left, return 0.
    
     
    
    Example 1:
    
    Input: stones = [2,7,4,1,8,1]
    Output: 1
    Explanation: 
    We combine 7 and 8 to get 1 so the array converts to [2,4,1,1,1] then,
    we combine 2 and 4 to get 2 so the array converts to [2,1,1,1] then,
    we combine 2 and 1 to get 1 so the array converts to [1,1,1] then,
    we combine 1 and 1 to get 0 so the array converts to [1] then that's the value of the last stone.
    Example 2:
    
    Input: stones = [1]
    Output: 1
     
    
    Constraints:
    
    1 <= stones.length <= 30
    1 <= stones[i] <= 1000

---
+++ 1046. Last Stone Weight.cpp
@@ -0,0 +1,32 @@
+Output Status: 
+
+Runtime: 3 ms, faster than 55.52% of C++ online submissions for Last Stone Weight.
+Memory Usage: 7.6 MB, less than 34.27% of C++ online submissions for Last Stone Weight.
+
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& s) {
+        
+        make_heap(s.begin(), s.end());
+        
+         while (s.size() > 1) {
+            int x = s.front();
+            pop_heap(s.begin(), s.end());
+            s.pop_back();
+            
+            int y = s.front();
+            pop_heap(s.begin(), s.end());
+            s.pop_back();
+            
+            if (x == y) continue;
+            
+            s.push_back(abs(x - y));
+            push_heap(s.begin(), s.end());
+        }
+        
+        return s.size() ? s.front() : 0;
+        
+    }
+};
+
+
