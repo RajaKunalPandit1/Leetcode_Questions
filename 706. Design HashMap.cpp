158a5c805f64bf0b6b5ebac4a8b948aea630c279
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2022-04-22 14:08:07 +0530

    Create 706. Design HashMap.cpp
    Output Status : 
    
    Runtime: 147 ms, faster than 75.36% of C++ online submissions for Design HashMap.
    Memory Usage: 61.1 MB, less than 52.13% of C++ online submissions for Design HashMap.
    
    Design a HashMap without using any built-in hash table libraries.
    
    Implement the MyHashMap class:
    
    MyHashMap() initializes the object with an empty map.
    void put(int key, int value) inserts a (key, value) pair into the HashMap. If the key already exists in the map, update the corresponding value.
    int get(int key) returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key.
    void remove(key) removes the key and its corresponding value if the map contains the mapping for the key.
     
    
    Example 1:
    
    Input
    ["MyHashMap", "put", "put", "get", "get", "put", "get", "remove", "get"]
    [[], [1, 1], [2, 2], [1], [3], [2, 1], [2], [2], [2]]
    Output
    [null, null, null, 1, -1, null, 1, null, -1]
    
    Explanation
    MyHashMap myHashMap = new MyHashMap();
    myHashMap.put(1, 1); // The map is now [[1,1]]
    myHashMap.put(2, 2); // The map is now [[1,1], [2,2]]
    myHashMap.get(1);    // return 1, The map is now [[1,1], [2,2]]
    myHashMap.get(3);    // return -1 (i.e., not found), The map is now [[1,1], [2,2]]
    myHashMap.put(2, 1); // The map is now [[1,1], [2,1]] (i.e., update the existing value)
    myHashMap.get(2);    // return 1, The map is now [[1,1], [2,1]]
    myHashMap.remove(2); // remove the mapping for 2, The map is now [[1,1]]
    myHashMap.get(2);    // return -1 (i.e., not found), The map is now [[1,1]]
     
    
    Constraints:
    
    0 <= key, value <= 106
    At most 104 calls will be made to put, get, and remove.
    
    Design a HashMap without using any built-in hash table libraries.
    
    Implement the MyHashMap class:
    
    MyHashMap() initializes the object with an empty map.
    void put(int key, int value) inserts a (key, value) pair into the HashMap. If the key already exists in the map, update the corresponding value.
    int get(int key) returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key.
    void remove(key) removes the key and its corresponding value if the map contains the mapping for the key.
     
    
    Example 1:
    
    Input
    ["MyHashMap", "put", "put", "get", "get", "put", "get", "remove", "get"]
    [[], [1, 1], [2, 2], [1], [3], [2, 1], [2], [2], [2]]
    Output
    [null, null, null, 1, -1, null, 1, null, -1]
    
    Explanation
    MyHashMap myHashMap = new MyHashMap();
    myHashMap.put(1, 1); // The map is now [[1,1]]
    myHashMap.put(2, 2); // The map is now [[1,1], [2,2]]
    myHashMap.get(1);    // return 1, The map is now [[1,1], [2,2]]
    myHashMap.get(3);    // return -1 (i.e., not found), The map is now [[1,1], [2,2]]
    myHashMap.put(2, 1); // The map is now [[1,1], [2,1]] (i.e., update the existing value)
    myHashMap.get(2);    // return 1, The map is now [[1,1], [2,1]]
    myHashMap.remove(2); // remove the mapping for 2, The map is now [[1,1]]
    myHashMap.get(2);    // return -1 (i.e., not found), The map is now [[1,1]]
     
    
    Constraints:
    
    0 <= key, value <= 106
    At most 104 calls will be made to put, get, and remove.

---
+++ 706. Design HashMap.cpp
@@ -0,0 +1,67 @@
+Output Status : 
+
+Runtime: 147 ms, faster than 75.36% of C++ online submissions for Design HashMap.
+Memory Usage: 61.1 MB, less than 52.13% of C++ online submissions for Design HashMap.
+
+class MyHashMap {
+public:
+   vector<vector<pair<int, int>>> map;
+	const int size = 10000;
+public:
+	/** Initialize your data structure here. */
+	MyHashMap() {
+		map.resize(size);
+	}
+
+	/** value will always be non-negative. */
+	void put(int key, int value) {
+		int index = key % size;
+        vector<pair<int, int>> &row = map[index];
+        for(auto iter = row.begin(); iter != row.end(); iter++)
+        {
+            if(iter->first == key)
+            {
+                iter->second = value;
+                return;
+            }
+        }
+		row.push_back(make_pair(key, value));
+	}
+
+	/** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+	int get(int key) {
+		int index = key % size;
+        vector<pair<int, int>> &row = map[index];
+		for (auto iter = row.begin(); iter != row.end(); iter++)
+		{
+			if (iter->first == key)
+			{
+				return iter->second;
+			}
+		}
+		return -1;
+	}
+
+	/** Removes the mapping of the specified value key if this map contains a mapping for the key */
+	void remove(int key) {
+		int index = key % size;
+        vector<pair<int, int>> &row = map[index];
+		for (auto iter = row.begin(); iter != row.end(); iter++)
+		{
+			if (iter->first == key)
+			{
+				row.erase(iter);
+                return;
+			}
+		}
+	}
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
+ 
