5ae14fc3feecc9e4acc3ac2244d816428821e177
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2022-05-01 06:23:52 +0530

    Create 844. Backspace String Compare.cpp
    Output Status: 
    
    Runtime: 0 ms, faster than 100.00% of C++ online submissions for Backspace String Compare.
    Memory Usage: 6.2 MB, less than 88.17% of C++ online submissions for Backspace String Compare.
    
    Given two strings s and t, return true if they are equal when both are typed into empty text editors. '#' means a backspace character.
    
    Note that after backspacing an empty text, the text will continue empty.
    
     
    
    Example 1:
    
    Input: s = "ab#c", t = "ad#c"
    Output: true
    Explanation: Both s and t become "ac".
    Example 2:
    
    Input: s = "ab##", t = "c#d#"
    Output: true
    Explanation: Both s and t become "".
    Example 3:
    
    Input: s = "a#c", t = "b"
    Output: false
    Explanation: s becomes "c" while t becomes "b".
     
    
    Constraints:
    
    1 <= s.length, t.length <= 200
    s and t only contain lowercase letters and '#' characters.
     
    
    Follow up: Can you solve it in O(n) time and O(1) space?

---
+++ 844. Backspace String Compare.cpp
@@ -0,0 +1,51 @@
+Output Status: 
+
+Runtime: 0 ms, faster than 100.00% of C++ online submissions for Backspace String Compare.
+Memory Usage: 6.2 MB, less than 88.17% of C++ online submissions for Backspace String Compare.
+
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        int k=0,p=0;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='#')
+            {
+                k--;
+                 k=max(0,k);
+            }
+            
+           else
+           {
+               s[k]=s[i];
+               k++;
+           }
+        }
+        for(int i=0;i<t.size();i++)
+        {
+            if(t[i]=='#')
+            {
+                p--;
+                 p=max(0,p);
+            }
+            
+           else
+           {
+               t[p]=t[i];
+               p++;
+           }
+        }
+        if(k!=p)
+            return false;
+        else
+        {
+            for(int i=0;i<k;i++)
+            {
+                if(s[i]!=t[i])
+                    return false;
+            }
+            return true;
+        }
+        
+    }
+};
