7446ab7dc439b0669db0118c5bc52737419cff12
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2022-05-12 13:14:45 +0530

    Create 47. Permutations II.cpp
    Output Status: 
    
    Runtime: 4 ms, faster than 91.86% of C++ online submissions for Permutations II.
    Memory Usage: 9 MB, less than 65.84% of C++ online submissions for Permutations II.
    
    Given a collection of numbers, nums, that might contain duplicates, return all possible unique permutations in any order.
    
     
    
    Example 1:
    
    Input: nums = [1,1,2]
    Output:
    [[1,1,2],
     [1,2,1],
     [2,1,1]]
    Example 2:
    
    Input: nums = [1,2,3]
    Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
     
    
    Constraints:
    
    1 <= nums.length <= 8
    -10 <= nums[i] <= 10

---
+++ 47. Permutations II.cpp
@@ -0,0 +1,26 @@
+Output Status: 
+
+Runtime: 4 ms, faster than 91.86% of C++ online submissions for Permutations II.
+Memory Usage: 9 MB, less than 65.84% of C++ online submissions for Permutations II.
+
+
+class Solution {
+public:
+    void recursion(vector<int> num, int i, int j, vector<vector<int> > &res) {
+        if (i == j-1) {
+            res.push_back(num);
+            return;
+        }
+        for (int k = i; k < j; k++) {
+            if (i != k && num[i] == num[k]) continue;
+            swap(num[i], num[k]);
+            recursion(num, i+1, j, res);
+        }
+    }
+    vector<vector<int> > permuteUnique(vector<int> &num) {
+        sort(num.begin(), num.end());
+        vector<vector<int> >res;
+        recursion(num, 0, num.size(), res);
+        return res;
+    }
+};
