dd1948299fe732ee9fef6b4b0e6d3d4125dcf759
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2022-05-24 19:18:15 +0530

    Create 32. Longest Valid Parentheses.cpp
    Output Status:
    
    Runtime: 8 ms, faster than 37.09% of C++ online submissions for Longest Valid Parentheses.
    Memory Usage: 7.4 MB, less than 42.55% of C++ online submissions for Longest Valid Parentheses.
    
    Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
    
     
    
    Example 1:
    
    Input: s = "(()"
    Output: 2
    Explanation: The longest valid parentheses substring is "()".
    Example 2:
    
    Input: s = ")()())"
    Output: 4
    Explanation: The longest valid parentheses substring is "()()".
    Example 3:
    
    Input: s = ""
    Output: 0
     
    
    Constraints:
    
    0 <= s.length <= 3 * 104
    s[i] is '(', or ')'.

---
+++ 32. Longest Valid Parentheses.cpp
@@ -0,0 +1,34 @@
+Output Status:
+
+Runtime: 8 ms, faster than 37.09% of C++ online submissions for Longest Valid Parentheses.
+Memory Usage: 7.4 MB, less than 42.55% of C++ online submissions for Longest Valid Parentheses.
+
+
+class Solution {
+public:
+   int longestValidParentheses(string s) {
+        int n = s.length(), longest = 0;
+        stack<int> st;
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '(') st.push(i);
+            else {
+                if (!st.empty()) {
+                    if (s[st.top()] == '(') st.pop();
+                    else st.push(i);
+                }
+                else st.push(i);
+            }
+        }
+        if (st.empty()) longest = n;
+        else {
+            int a = n, b = 0;
+            while (!st.empty()) {
+                b = st.top(); st.pop();
+                longest = max(longest, a-b-1);
+                a = b;
+            }
+            longest = max(longest, a);
+        }
+        return longest;
+    }
+};
