929109c1a3ba92f1f48cbe9e6a6ca4fdfb11086f
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2022-06-16 10:43:45 +0530

    Create 5. Longest Palindromic Substring.cpp
    Output Status :
    
    Runtime: 19 ms, faster than 92.07% of C++ online submissions for Longest Palindromic Substring.
    Memory Usage: 7.2 MB, less than 81.58% of C++ online submissions for Longest Palindromic Substring.
    
    Given a string s, return the longest palindromic substring in s.
    
    Example 1:
    
    Input: s = "babad"
    Output: "bab"
    Explanation: "aba" is also a valid answer.
    Example 2:
    
    Input: s = "cbbd"
    Output: "bb"
     
    
    Constraints:
    
    1 <= s.length <= 1000
    s consist of only digits and English letters.

---
+++ 5. Longest Palindromic Substring.cpp
@@ -0,0 +1,64 @@
+Output Status :
+
+Runtime: 19 ms, faster than 92.07% of C++ online submissions for Longest Palindromic Substring.
+Memory Usage: 7.2 MB, less than 81.58% of C++ online submissions for Longest Palindromic Substring.
+
+
+class Solution {
+public:
+    
+    string longestPalindrome(string s) {
+        
+        int n = s.length();
+        
+        if(n<=1) return s;
+        
+        int max_len = 1;
+        int start  = 0, end = 0;
+        
+        for(int i=0;i<n-1;i++){
+            
+            int low = i,high = i;
+            
+            while(low>=0 && high < n){
+                
+                if(s[low] == s[high]){
+                    low--;
+                    high++;
+                }else{
+                    break;
+                }
+            }
+            int len = high-low-1;
+            
+            if(len>max_len){
+                max_len = len;
+                start = low+1;
+                end = high-1;
+            }
+        }
+        
+        for(int i=0;i<n-1;i++){
+            
+            int low = i,high = i+1;
+            
+            while(low>=0 && high < n){
+                
+                if(s[low] == s[high]){
+                    low--;
+                    high++;
+                }else{
+                    break;
+                }
+            }
+            int len = high-low-1;
+            
+            if(len>max_len){
+                max_len = len;
+                start = low+1;
+                end = high-1;
+            }
+        }
+        return s.substr(start,max_len);
+    }
+};
