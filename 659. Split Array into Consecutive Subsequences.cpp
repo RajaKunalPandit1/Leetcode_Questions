a9e5f0ef4666781c24c72b886ea0f5783746d803
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2022-08-19 18:38:40 +0530

    Create 659. Split Array into Consecutive Subsequences.cpp
    Output Status : 
    
    Runtime: 158 ms, faster than 54.90% of C++ online submissions for Split Array into Consecutive Subsequences.
    Memory Usage: 58.5 MB, less than 27.74% of C++ online submissions for Split Array into Consecutive Subsequences.
    
    You are given an integer array nums that is sorted in non-decreasing order.
    
    Determine if it is possible to split nums into one or more subsequences such that both of the following conditions are true:
    
    Each subsequence is a consecutive increasing sequence (i.e. each integer is exactly one more than the previous integer).
    All subsequences have a length of 3 or more.
    Return true if you can split nums according to the above conditions, or false otherwise.
    
    A subsequence of an array is a new array that is formed from the original array by deleting some (can be none) of the elements without disturbing the relative positions of the remaining elements. (i.e., [1,3,5] is a subsequence of [1,2,3,4,5] while [1,3,2] is not).
    
     
    
    Example 1:
    
    Input: nums = [1,2,3,3,4,5]
    Output: true
    Explanation: nums can be split into the following subsequences:
    [1,2,3,3,4,5] --> 1, 2, 3
    [1,2,3,3,4,5] --> 3, 4, 5
    Example 2:
    
    Input: nums = [1,2,3,3,4,4,5,5]
    Output: true
    Explanation: nums can be split into the following subsequences:
    [1,2,3,3,4,4,5,5] --> 1, 2, 3, 4, 5
    [1,2,3,3,4,4,5,5] --> 3, 4, 5
    Example 3:
    
    Input: nums = [1,2,3,4,4,5]
    Output: false
    Explanation: It is impossible to split nums into consecutive increasing subsequences of length 3 or more.
     
    
    Constraints:
    
    1 <= nums.length <= 104
    -1000 <= nums[i] <= 1000
    nums is sorted in non-decreasing order.

---
+++ 659. Split Array into Consecutive Subsequences.cpp
@@ -0,0 +1,30 @@
+Output Status : 
+
+Runtime: 158 ms, faster than 54.90% of C++ online submissions for Split Array into Consecutive Subsequences.
+Memory Usage: 58.5 MB, less than 27.74% of C++ online submissions for Split Array into Consecutive Subsequences.
+
+
+class Solution {
+public:
+    bool isPossible(vector<int>& A) {
+         unordered_map<int, int> left, end;
+        for (int i: A) {
+            left[i]++;
+        }
+        for (int i: A) {
+            if (left[i] == 0) continue;
+            left[i]--;
+            if (end[i - 1] > 0) {   
+                end[i - 1]--;
+                end[i]++;
+            } else if (left[i + 1] > 0 && left[i + 2] > 0) {
+                left[i + 1]--;
+                left[i + 2]--;
+                end[i + 2]++;
+            } else {   
+                return false;
+            }
+        }
+        return true;
+    }
+};
