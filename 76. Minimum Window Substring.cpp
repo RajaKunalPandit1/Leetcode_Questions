5bfd210c21023c2b3652401413f9cbd9ae88cfc3
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2022-10-22 18:12:41 +0530

    Create 76. Minimum Window Substring.cpp
    Output Status : 
    
    Runtime: 19 ms, faster than 80.58% of C++ online submissions for Minimum Window Substring.
    Memory Usage: 7.7 MB, less than 68.10% of C++ online submissions for Minimum Window Substring.
    
    Given two strings s and t of lengths m and n respectively, return the minimum window substring of s such that every character in t (including duplicates) is included in the window. If there is no such substring, return the empty string "".
    
    The testcases will be generated such that the answer is unique.
    
    A substring is a contiguous sequence of characters within the string.
    
     
    
    Example 1:
    
    Input: s = "ADOBECODEBANC", t = "ABC"
    Output: "BANC"
    Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string t.
    Example 2:
    
    Input: s = "a", t = "a"
    Output: "a"
    Explanation: The entire string s is the minimum window.
    Example 3:
    
    Input: s = "a", t = "aa"
    Output: ""
    Explanation: Both 'a's from t must be included in the window.
    Since the largest window of s only has one 'a', return empty string.
     
    
    Constraints:
    
    m == s.length
    n == t.length
    1 <= m, n <= 105
    s and t consist of uppercase and lowercase English letters.
     
    
    Follow up: Could you find an algorithm that runs in O(m + n) time?

---
+++ 76. Minimum Window Substring.cpp
@@ -0,0 +1,33 @@
+Output Status : 
+
+Runtime: 19 ms, faster than 80.58% of C++ online submissions for Minimum Window Substring.
+Memory Usage: 7.7 MB, less than 68.10% of C++ online submissions for Minimum Window Substring.
+
+class Solution {
+public:
+    string minWindow(string s, string t) {
+           if (s.size() == 0 || t.size() == 0) return "";
+        vector<int> remaining(128, 0);
+        int required = t.size();
+        for (int i = 0; i < required; i++) remaining[t[i]]++;
+        // left is the start index of the min-length substring ever found
+        int min = INT_MAX, start = 0, left = 0, i = 0;
+        while(i <= s.size() && start < s.size()) {
+            if(required) {
+                if (i == s.size()) break;
+                remaining[s[i]]--;
+                if (remaining[s[i]] >= 0) required--;
+                i++;
+            } else {
+                if (i - start < min) {
+                    min = i -start;
+                    left = start;
+                }
+                remaining[s[start]]++;
+                if (remaining[s[start]] > 0) required++;
+                start++;
+            }
+        }
+        return min == INT_MAX? "" : s.substr(left, min);
+    }
+};
