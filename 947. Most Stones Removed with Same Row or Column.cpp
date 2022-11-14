d80546c21f7ab07e0004d6ec22bfdb2443ff2420
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2022-11-14 14:08:26 +0530

    Create 947. Most Stones Removed with Same Row or Column.cpp
    Output Status : 
    
    Runtime: 111 ms, faster than 72.37% of C++ online submissions for Most Stones Removed with Same Row or Column.
    Memory Usage: 16.6 MB, less than 55.74% of C++ online submissions for Most Stones Removed with Same Row or Column.
    
    On a 2D plane, we place n stones at some integer coordinate points. Each coordinate point may have at most one stone.
    
    A stone can be removed if it shares either the same row or the same column as another stone that has not been removed.
    
    Given an array stones of length n where stones[i] = [xi, yi] represents the location of the ith stone, return the largest possible number of stones that can be removed.
    
     
    
    Example 1:
    
    Input: stones = [[0,0],[0,1],[1,0],[1,2],[2,1],[2,2]]
    Output: 5
    Explanation: One way to remove 5 stones is as follows:
    1. Remove stone [2,2] because it shares the same row as [2,1].
    2. Remove stone [2,1] because it shares the same column as [0,1].
    3. Remove stone [1,2] because it shares the same row as [1,0].
    4. Remove stone [1,0] because it shares the same column as [0,0].
    5. Remove stone [0,1] because it shares the same row as [0,0].
    Stone [0,0] cannot be removed since it does not share a row/column with another stone still on the plane.
    Example 2:
    
    Input: stones = [[0,0],[0,2],[1,1],[2,0],[2,2]]
    Output: 3
    Explanation: One way to make 3 moves is as follows:
    1. Remove stone [2,2] because it shares the same row as [2,0].
    2. Remove stone [2,0] because it shares the same column as [0,0].
    3. Remove stone [0,2] because it shares the same row as [0,0].
    Stones [0,0] and [1,1] cannot be removed since they do not share a row/column with another stone still on the plane.
    Example 3:
    
    Input: stones = [[0,0]]
    Output: 0
    Explanation: [0,0] is the only stone on the plane, so you cannot remove it.
     
    
    Constraints:
    
    1 <= stones.length <= 1000
    0 <= xi, yi <= 104
    No two stones are at the same coordinate point.

---
+++ 947. Most Stones Removed with Same Row or Column.cpp
@@ -0,0 +1,27 @@
+Output Status : 
+
+Runtime: 111 ms, faster than 72.37% of C++ online submissions for Most Stones Removed with Same Row or Column.
+Memory Usage: 16.6 MB, less than 55.74% of C++ online submissions for Most Stones Removed with Same Row or Column.
+
+class Solution {
+public:
+        int removeStones(vector<vector<int>>& stones) {
+        for (int i = 0; i < stones.size(); ++i)
+            uni(stones[i][0], ~stones[i][1]);
+        return stones.size() - islands;
+    }
+
+    unordered_map<int, int> f;
+    int islands = 0;
+
+    int find(int x) {
+        if (!f.count(x)) f[x] = x, islands++;
+        if (x != f[x]) f[x] = find(f[x]);
+        return f[x];
+    }
+
+    void uni(int x, int y) {
+        x = find(x), y = find(y);
+        if (x != y) f[x] = y, islands--;
+    }
+};
