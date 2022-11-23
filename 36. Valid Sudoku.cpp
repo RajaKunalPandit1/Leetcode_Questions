a4d2ce8c50247a9a5844f5553c80906bda179a41
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2022-11-23 14:10:54 +0530

    Create 36. Valid Sudoku.cpp
    Output Status : 
    
    Runtime: 42 ms, faster than 63.50% of C++ online submissions for Valid Sudoku.
    Memory Usage: 18.5 MB, less than 60.98% of C++ online submissions for Valid Sudoku.
    
    Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
    
    Each row must contain the digits 1-9 without repetition.
    Each column must contain the digits 1-9 without repetition.
    Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
    Note:
    
    A Sudoku board (partially filled) could be valid but is not necessarily solvable.
    Only the filled cells need to be validated according to the mentioned rules.
     
    
    Example 1:
    
    
    Input: board = 
    [["5","3",".",".","7",".",".",".","."]
    ,["6",".",".","1","9","5",".",".","."]
    ,[".","9","8",".",".",".",".","6","."]
    ,["8",".",".",".","6",".",".",".","3"]
    ,["4",".",".","8",".","3",".",".","1"]
    ,["7",".",".",".","2",".",".",".","6"]
    ,[".","6",".",".",".",".","2","8","."]
    ,[".",".",".","4","1","9",".",".","5"]
    ,[".",".",".",".","8",".",".","7","9"]]
    Output: true
    Example 2:
    
    Input: board = 
    [["8","3",".",".","7",".",".",".","."]
    ,["6",".",".","1","9","5",".",".","."]
    ,[".","9","8",".",".",".",".","6","."]
    ,["8",".",".",".","6",".",".",".","3"]
    ,["4",".",".","8",".","3",".",".","1"]
    ,["7",".",".",".","2",".",".",".","6"]
    ,[".","6",".",".",".",".","2","8","."]
    ,[".",".",".","4","1","9",".",".","5"]
    ,[".",".",".",".","8",".",".","7","9"]]
    Output: false
    Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
     
    
    Constraints:
    
    board.length == 9
    board[i].length == 9
    board[i][j] is a digit 1-9 or '.'.

---
+++ 36. Valid Sudoku.cpp
@@ -0,0 +1,30 @@
+Output Status : 
+
+Runtime: 42 ms, faster than 63.50% of C++ online submissions for Valid Sudoku.
+Memory Usage: 18.5 MB, less than 60.98% of C++ online submissions for Valid Sudoku.
+
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+        vector<int>row(9,0);
+        vector<int>col(9,0);
+        vector<vector<int>>grid(3,vector<int>(3,0));
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                    int val=mat[i][j]=='.'?0:1<<(mat[i][j]-'0');
+                    if(row[i]&val||col[j]&val||grid[i/3][j/3]&val)
+                    {
+                        return false;
+                    }
+                    row[i]|=val;
+                    col[j]|=val;
+                    grid[i/3][j/3]|=val;
+            }
+        }
+        return true;
+    }
+};
