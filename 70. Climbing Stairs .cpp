a1d4fa3185a92ffe972b2522f0cb65bfbdcc1fe6
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2022-12-12 20:35:09 +0530

    Create 70. Climbing Stairs .cpp
    Output Status :
    
    Runtime
    0 ms
    Beats
    100%
    Memory
    6.2 MB
    Beats
    29.54%
    
    You are climbing a staircase. It takes n steps to reach the top.
    
    Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
    
     
    
    Example 1:
    
    Input: n = 2
    Output: 2
    Explanation: There are two ways to climb to the top.
    1. 1 step + 1 step
    2. 2 steps
    Example 2:
    
    Input: n = 3
    Output: 3
    Explanation: There are three ways to climb to the top.
    1. 1 step + 1 step + 1 step
    2. 1 step + 2 steps
    3. 2 steps + 1 step
     
    
    Constraints:
    
    1 <= n <= 45

---
+++ 70. Climbing Stairs .cpp
@@ -0,0 +1,31 @@
+Output Status :
+
+Runtime
+0 ms
+Beats
+100%
+Memory
+6.2 MB
+Beats
+29.54%
+
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n+1,-1);
+        
+        if(n == 0 || n == 1){    
+            return 1;
+        }
+        
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        
+        return dp[n];
+    }
+};
