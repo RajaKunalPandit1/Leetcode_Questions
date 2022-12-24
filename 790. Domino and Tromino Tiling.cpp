b1aa1e5527089443096005325948d4114bb95d97
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2022-12-24 10:10:59 +0530

    Create 790. Domino and Tromino Tiling.cpp
    Output Status :
    
    Runtime
    0 ms
    Beats
    100%
    Memory
    6.6 MB
    Beats
    40.40%
    
    You have two types of tiles: a 2 x 1 domino shape and a tromino shape. You may rotate these shapes.
    
    
    Given an integer n, return the number of ways to tile an 2 x n board. Since the answer may be very large, return it modulo 109 + 7.
    
    In a tiling, every square must be covered by a tile. Two tilings are different if and only if there are two 4-directionally adjacent cells on the board such that exactly one of the tilings has both squares occupied by a tile.
    
     
    
    Example 1:
    
    
    Input: n = 3
    Output: 5
    Explanation: The five different ways are show above.
    Example 2:
    
    Input: n = 1
    Output: 1
     
    
    Constraints:
    
    1 <= n <= 1000

---
+++ 790. Domino and Tromino Tiling.cpp
@@ -0,0 +1,31 @@
+Output Status :
+
+Runtime
+0 ms
+Beats
+100%
+Memory
+6.6 MB
+Beats
+40.40%
+
+class Solution {
+public:
+    int numTilings(int n) {
+        
+        if (n < 3) return n;
+        
+        int mod = pow(10, 9) + 7;
+        vector<long> D(n+1, 0), T(n+1, 0);
+        D[0] = 0, D[1] = 1, D[2] = 2;
+        T[0] = 0, T[1] = 1, T[2] = 2;
+        
+        for (int i = 3; i <= n; i++) {
+            D[i] = (D[i-1] + D[i-2] + 2*T[i-2]) % mod;
+            T[i] = (T[i-1] + D[i-1]) % mod;
+        }
+        
+        return D[n];
+        
+    }
+};
