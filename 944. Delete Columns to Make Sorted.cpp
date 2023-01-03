d06ac586f50e08d4b5c9bee35bff9446475760ad
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2023-01-03 21:06:52 +0530

    Create 944. Delete Columns to Make Sorted.cpp
    Output Status : 
    
    Runtime
    55 ms
    Beats
    66.25%
    Memory
    12.2 MB
    Beats
    72.46%
    
    You are given an array of n strings strs, all of the same length.
    
    The strings can be arranged such that there is one on each line, making a grid. For example, strs = ["abc", "bce", "cae"] can be arranged as:
    
    abc
    bce
    cae
    You want to delete the columns that are not sorted lexicographically. In the above example (0-indexed), columns 0 ('a', 'b', 'c') and 2 ('c', 'e', 'e') are sorted while column 1 ('b', 'c', 'a') is not, so you would delete column 1.
    
    Return the number of columns that you will delete.
    
     
    
    Example 1:
    
    Input: strs = ["cba","daf","ghi"]
    Output: 1
    Explanation: The grid looks as follows:
      cba
      daf
      ghi
    Columns 0 and 2 are sorted, but column 1 is not, so you only need to delete 1 column.
    Example 2:
    
    Input: strs = ["a","b"]
    Output: 0
    Explanation: The grid looks as follows:
      a
      b
    Column 0 is the only column and is sorted, so you will not delete any columns.
    Example 3:
    
    Input: strs = ["zyx","wvu","tsr"]
    Output: 3
    Explanation: The grid looks as follows:
      zyx
      wvu
      tsr
    All 3 columns are not sorted, so you will delete all 3.
     
    
    Constraints:
    
    n == strs.length
    1 <= n <= 100
    1 <= strs[i].length <= 1000
    strs[i] consists of lowercase English letters.

---
+++ 944. Delete Columns to Make Sorted.cpp
@@ -0,0 +1,31 @@
+Output Status : 
+
+Runtime
+55 ms
+Beats
+66.25%
+Memory
+12.2 MB
+Beats
+72.46%
+
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        
+        int res = 0;
+        int row = strs.size();
+        int col = strs[0].size();
+
+        for(int j=0;j<col;j++){
+            for(int i=0;i<row-1;i++){
+                if(strs[i][j] > strs[i+1][j]){
+                    res++;
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+};
+
