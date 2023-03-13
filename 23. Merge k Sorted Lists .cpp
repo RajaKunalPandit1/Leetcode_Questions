8a083dc5429ae5913ed96660f2da4a4bbd13c7c1
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2023-03-13 10:35:26 +0530

    Create 23. Merge k Sorted Lists .cpp
    Output Status : 
    
    Runtime
    25 ms
    Beats
    68.75%
    Memory
    14.1 MB
    Beats
    10.73%
    
    You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
    
    Merge all the linked-lists into one sorted linked-list and return it.
    
     
    
    Example 1:
    
    Input: lists = [[1,4,5],[1,3,4],[2,6]]
    Output: [1,1,2,3,4,4,5,6]
    Explanation: The linked-lists are:
    [
      1->4->5,
      1->3->4,
      2->6
    ]
    merging them into one sorted list:
    1->1->2->3->4->4->5->6
    Example 2:
    
    Input: lists = []
    Output: []
    Example 3:
    
    Input: lists = [[]]
    Output: []
     
    
    Constraints:
    
    k == lists.length
    0 <= k <= 104
    0 <= lists[i].length <= 500
    -104 <= lists[i][j] <= 104
    lists[i] is sorted in ascending order.
    The sum of lists[i].length will not exceed 104.

---
+++ 23. Merge k Sorted Lists .cpp
@@ -0,0 +1,63 @@
+Output Status : 
+
+Runtime
+25 ms
+Beats
+68.75%
+Memory
+14.1 MB
+Beats
+10.73%
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        
+         if(lists.empty()) return nullptr;
+        
+        priority_queue<pair<int,ListNode*>> pq;
+        int n=lists.size();
+        
+        for(int i=0;i<n;i++)
+        {
+            while(lists[i]!=nullptr)
+            {
+                pq.push({-lists[i]->val,lists[i]});
+                lists[i]=lists[i]->next;
+            }
+        }
+        
+        
+        ListNode* head=nullptr;
+        ListNode* k=nullptr;
+        
+        if(pq.empty()) return head;
+        
+        head=pq.top().second;
+        k=pq.top().second;
+        pq.pop();
+        
+     
+        while(!pq.empty())
+        {
+            k->next=pq.top().second;
+            k=pq.top().second;
+            pq.pop();
+            
+        }
+        k->next=NULL;
+        
+        return head==NULL?nullptr:head;
+        
+    }
+};
