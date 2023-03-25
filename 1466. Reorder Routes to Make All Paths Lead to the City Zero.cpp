156e4675e95b0fd7aad514647b899c6bf169dafa
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2023-03-25 19:33:01 +0530

    Create 1466. Reorder Routes to Make All Paths Lead to the City Zero.cpp
    Output Status : 
    
    Runtime
    358 ms
    Beats
    83.61%
    Memory
    106.3 MB
    Beats
    76.22%
    
    There are n cities numbered from 0 to n - 1 and n - 1 roads such that there is only one way to travel between two different cities (this network form a tree). Last year, The ministry of transport decided to orient the roads in one direction because they are too narrow.
    
    Roads are represented by connections where connections[i] = [ai, bi] represents a road from city ai to city bi.
    
    This year, there will be a big event in the capital (city 0), and many people want to travel to this city.
    
    Your task consists of reorienting some roads such that each city can visit the city 0. Return the minimum number of edges changed.
    
    It's guaranteed that each city can reach city 0 after reorder.
    
     
    
    Example 1:
    
    
    Input: n = 6, connections = [[0,1],[1,3],[2,3],[4,0],[4,5]]
    Output: 3
    Explanation: Change the direction of edges show in red such that each node can reach the node 0 (capital).
    Example 2:
    
    
    Input: n = 5, connections = [[1,0],[1,2],[3,2],[3,4]]
    Output: 2
    Explanation: Change the direction of edges show in red such that each node can reach the node 0 (capital).
    Example 3:
    
    Input: n = 3, connections = [[1,0],[2,0]]
    Output: 0
     
    
    Constraints:
    
    2 <= n <= 5 * 104
    connections.length == n - 1
    connections[i].length == 2
    0 <= ai, bi <= n - 1
    ai != bi

---
+++ 1466. Reorder Routes to Make All Paths Lead to the City Zero.cpp
@@ -0,0 +1,30 @@
+Output Status : 
+
+Runtime
+358 ms
+Beats
+83.61%
+Memory
+106.3 MB
+Beats
+76.22%
+
+class Solution {
+public:
+    int dfs(vector<vector<int>> &gr, vector<bool> &visited, int from) {
+    auto change = 0;
+    visited[from] = true;
+    for (auto to : gr[from])
+        if (!visited[abs(to)])
+            change += dfs(gr, visited, abs(to)) + (to > 0);
+    return change;        
+    }
+    int minReorder(int n, vector<vector<int>>& connections) {
+        vector<vector<int>> gr(n);
+        for (auto &c : connections) {
+            gr[c[0]].push_back(c[1]);
+            gr[c[1]].push_back(-c[0]);
+        }
+        return dfs(gr, vector<bool>(n) = {}, 0);
+    }
+};
