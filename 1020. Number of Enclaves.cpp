ba3f1c47e85e06a0288a7a5df98bef71eed1161c
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2023-04-07 22:38:36 +0530

    Create 1020. Number of Enclaves.cpp
    Output Status : 
    
    Runtime
    343 ms
    Beats
    5.1%
    Memory
    119.1 MB
    Beats
    5.1%
    
    You are given an m x n binary matrix grid, where 0 represents a sea cell and 1 represents a land cell.
    
    A move consists of walking from one land cell to another adjacent (4-directionally) land cell or walking off the boundary of the grid.
    
    Return the number of land cells in grid for which we cannot walk off the boundary of the grid in any number of moves.
    
     
    
    Example 1:
    
    
    Input: grid = [[0,0,0,0],[1,0,1,0],[0,1,1,0],[0,0,0,0]]
    Output: 3
    Explanation: There are three 1s that are enclosed by 0s, and one 1 that is not enclosed because its on the boundary.
    Example 2:
    
    
    Input: grid = [[0,1,1,0],[0,0,1,0],[0,0,1,0],[0,0,0,0]]
    Output: 0
    Explanation: All 1s are either on the boundary or can reach the boundary.
     
    
    Constraints:
    
    m == grid.length
    n == grid[i].length
    1 <= m, n <= 500
    grid[i][j] is either 0 or 1.

---
+++ 1020. Number of Enclaves.cpp
@@ -0,0 +1,39 @@
+Output Status : 
+
+Runtime
+343 ms
+Beats
+5.1%
+Memory
+119.1 MB
+Beats
+5.1%
+
+class Solution {
+public:
+    int numEnclaves(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if ((i == 0 || j == 0 || i == m - 1 || j == n - 1) && grid[i][j] == 1) {
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return accumulate(begin(grid), end(grid), 0, [](int sum, auto row) { return sum + accumulate(begin(row), end(row), 0); });
+    }
+    
+private:
+    void dfs(vector<vector<int>>& grid, int i, int j) {
+        grid[i][j] = 0;
+        vector<vector<int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        for (vector<int>& direction : directions) {
+            int x = i + direction[0];
+            int y = j + direction[1];
+            if (x >= 0 && x < grid.size() && y >= 0 && y < grid[0].size() && grid[x][y] == 1) {
+                dfs(grid, x, y);
+            }
+        }
+    }
+};
