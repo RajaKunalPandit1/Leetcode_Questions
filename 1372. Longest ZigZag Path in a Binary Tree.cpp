0d09b40914c72f3f01994cb6e8a6ada17caac176
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2023-04-20 11:50:10 +0530

    Create 1372. Longest ZigZag Path in a Binary Tree.cpp
    Output Status : 
    
    Runtime185 ms
    Beats
    40.28%
    Memory94.2 MB
    Beats
    63.22%
    
    You are given the root of a binary tree.
    
    A ZigZag path for a binary tree is defined as follow:
    
        Choose any node in the binary tree and a direction (right or left).
        If the current direction is right, move to the right child of the current node; otherwise, move to the left child.
        Change the direction from right to left or from left to right.
        Repeat the second and third steps until you can't move in the tree.
    
    Zigzag length is defined as the number of nodes visited - 1. (A single node has a length of 0).
    
    Return the longest ZigZag path contained in that tree.
    
     
    
    Example 1:
    
    Input: root = [1,null,1,1,1,null,null,1,1,null,1,null,null,null,1,null,1]
    Output: 3
    Explanation: Longest ZigZag path in blue nodes (right -> left -> right).
    
    Example 2:
    
    Input: root = [1,1,1,null,1,null,null,1,1,null,1]
    Output: 4
    Explanation: Longest ZigZag path in blue nodes (left -> right -> left -> right).
    
    Example 3:
    
    Input: root = [1]
    Output: 0
    
     
    
    Constraints:
    
        The number of nodes in the tree is in the range [1, 5 * 104].
        1 <= Node.val <= 100

---
+++ 1372. Longest ZigZag Path in a Binary Tree.cpp
@@ -0,0 +1,37 @@
+Output Status : 
+
+Runtime185 ms
+Beats
+40.28%
+Memory94.2 MB
+Beats
+63.22%
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    public:
+    int maxLength=0;
+
+    void solve(TreeNode* root,int dir,int currLength){
+        if(!root) return;
+        maxLength=max(maxLength,currLength);
+        solve(root->left,0,dir?currLength+1:1);
+        solve(root->right,1,dir?1:currLength+1);
+    }
+
+    int longestZigZag(TreeNode* root) {
+        solve(root,0,0);
+        solve(root,1,0);
+        return maxLength;
+    }
+};
