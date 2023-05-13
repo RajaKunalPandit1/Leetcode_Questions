2bddb512bc5dc2f18c5c3fa69bbb6ed441292ae2
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2023-05-13 19:30:34 +0530

    Create 2466. Count Ways To Build Good Strings.cpp
    Given the integers zero, one, low, and high, we can construct a string by starting with an empty string, and then at each step perform either of the following:
    
    Append the character '0' zero times.
    Append the character '1' one times.
    This can be performed any number of times.
    
    A good string is a string constructed by the above process having a length between low and high (inclusive).
    
    Return the number of different good strings that can be constructed satisfying these properties. Since the answer can be large, return it modulo 109 + 7.
    
     
    
    Example 1:
    
    Input: low = 3, high = 3, zero = 1, one = 1
    Output: 8
    Explanation: 
    One possible valid good string is "011". 
    It can be constructed as follows: "" -> "0" -> "01" -> "011". 
    All binary strings from "000" to "111" are good strings in this example.
    Example 2:
    
    Input: low = 2, high = 3, zero = 1, one = 2
    Output: 5
    Explanation: The good strings are "00", "11", "000", "110", and "011".
     
    
    Constraints:
    
    1 <= low <= high <= 105
    1 <= zero, one <= low

---
+++ 2466. Count Ways To Build Good Strings.cpp
@@ -0,0 +1,33 @@
+Output Status : 
+
+Runtime
+13 ms
+Beats
+80.23%
+Memory
+9.4 MB
+Beats
+81.95%
+
+class Solution {
+public:
+    int countGoodStrings(int low, int high, int zero, int one) {
+        vector<int> dp(high + 1);
+        dp[0] = 1;
+        int md = 1e9 + 7;
+        for (int i = min(zero, one); i <= high; i++) {
+            if(i>=zero){
+                dp[i] = (dp[i] + dp[i - zero]) % md;
+            }
+            if(i>=one){
+                dp[i] = (dp[i] + dp[i - one]) % md;
+            }
+            
+        }
+        int sum = 0;
+        for (int i = low; i <= high; i++) {
+            sum = (sum + dp[i]) % md;
+        }
+        return sum;
+    }
+};
