267404c4b4bda9655b6592cb3456d8d4ec7e2aab
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2023-06-20 22:35:50 +0530

    Create 700. Search in a Binary Search Tree .cpp
    You are given the root of a binary search tree (BST) and an integer val.
    
    Find the node in the BST that the node's value equals val and return the subtree rooted with that node. If such a node does not exist, return null.
    
     
    
    Example 1:
    
    
    Input: root = [4,2,7,1,3], val = 2
    Output: [2,1,3]
    Example 2:
    
    
    Input: root = [4,2,7,1,3], val = 5
    Output: []
     
    
    Constraints:
    
    The number of nodes in the tree is in the range [1, 5000].
    1 <= Node.val <= 107
    root is a binary search tree.
    1 <= val <= 107

---
+++ 700. Search in a Binary Search Tree .cpp
@@ -0,0 +1,74 @@
+Output Status :
+
+Runtime
+26 ms
+Beats
+99.78%
+Memory
+34.6 MB
+Beats
+99.57%
+
+// Recursive
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        
+        if(root == NULL) return NULL;
+
+        if(root->val == val){
+            return root;
+        }elseif(root->val < val){
+
+            return searchBST(root->right,val);
+        }else{
+            return searchBST(root->left,val);
+        }
+        
+    }
+};
+
+// Iterative 
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        
+        if(root == NULL) return NULL;
+
+        while(root){
+
+            if(root->val == val){
+                return root;
+            }else if(root->val > val){
+                root = root->left;
+            }else{
+                root = root->right;
+            }
+        }
+        return NULL;
+    }
+};
