8c0be2e2bcd7f22a446be50a24e28d2398efd7f1
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2023-06-23 21:33:59 +0530

    Create 1027. Longest Arithmetic Subsequence.cpp
    Given an array nums of integers, return the length of the longest arithmetic subsequence in nums.
    
    Note that:
    
    A subsequence is an array that can be derived from another array by deleting some or no elements without changing the order of the remaining elements.
    A sequence seq is arithmetic if seq[i + 1] - seq[i] are all the same value (for 0 <= i < seq.length - 1).
     
    
    Example 1:
    
    Input: nums = [3,6,9,12]
    Output: 4
    Explanation:  The whole array is an arithmetic sequence with steps of length = 3.
    Example 2:
    
    Input: nums = [9,4,7,2,10]
    Output: 3
    Explanation:  The longest arithmetic subsequence is [4,7,10].
    Example 3:
    
    Input: nums = [20,1,15,3,10,5,8]
    Output: 4
    Explanation:  The longest arithmetic subsequence is [20,15,10,5].
     
    
    Constraints:
    
    2 <= nums.length <= 1000
    0 <= nums[i] <= 500

---
+++ 1027. Longest Arithmetic Subsequence.cpp
@@ -0,0 +1,26 @@
+Output Status : 
+
+Runtime
+2893 ms
+Beats
+5.5%
+Memory
+246.1 MB
+Beats
+19.3%
+
+class Solution {
+public:
+    int longestArithSeqLength(vector<int>& A) {
+        unordered_map<int, unordered_map<int, int>> dp;
+        int res = 2, n = A.size();
+        for (int i = 0; i < n; ++i)
+            for (int j = i + 1; j < n; ++j)  {
+                int d = A[j] - A[i];
+                dp[d][j] = dp[d].count(i) ? dp[d][i] + 1 : 2;
+                res = max(res, dp[d][j]);
+            }
+
+        return res;
+    }
+};
