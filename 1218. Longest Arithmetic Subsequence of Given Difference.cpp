75efa9c3652d41a63cbfd2a6df78a0f6c1229429
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2023-07-14 07:23:39 +0530

    Create 1218. Longest Arithmetic Subsequence of Given Difference.cpp
    Given an integer array arr and an integer difference, return the length of the longest subsequence in arr which is an arithmetic sequence such that the difference between adjacent elements in the subsequence equals difference.
    
    A subsequence is a sequence that can be derived from arr by deleting some or no elements without changing the order of the remaining elements.
    
     
    
    Example 1:
    
    Input: arr = [1,2,3,4], difference = 1
    Output: 4
    Explanation: The longest arithmetic subsequence is [1,2,3,4].
    Example 2:
    
    Input: arr = [1,3,5,7], difference = 1
    Output: 1
    Explanation: The longest arithmetic subsequence is any single element.
    Example 3:
    
    Input: arr = [1,5,7,8,5,3,4,2,1], difference = -2
    Output: 4
    Explanation: The longest arithmetic subsequence is [7,5,3,1].
     
    
    Constraints:
    
    1 <= arr.length <= 105
    -104 <= arr[i], difference <= 104

---
+++ 1218. Longest Arithmetic Subsequence of Given Difference.cpp
@@ -0,0 +1,25 @@
+Output Status : 
+
+Runtime
+156 ms
+Beats
+78.13%
+Memory
+54.8 MB
+Beats
+68.37%
+
+class Solution {
+public:
+    int longestSubsequence(vector<int>& arr, int difference) {
+      int ans = 1;
+      unordered_map<int,int>dp;
+      for(auto &val : arr)
+      {
+        if(!dp.count(val - difference)) dp[val] = 1;
+        else dp[val] = dp[val - difference] + 1;
+        ans = max(ans, dp[val]);
+      }
+      return ans;
+    }
+};
