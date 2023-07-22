fe233205446285a982da597cead5e33b14e5ec0e
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2023-07-22 09:51:54 +0530

    Create 688. Knight Probability in Chessboard.cpp
    On an n x n chessboard, a knight starts at the cell (row, column) and attempts to make exactly k moves. The rows and columns are 0-indexed, so the top-left cell is (0, 0), and the bottom-right cell is (n - 1, n - 1).
    
    A chess knight has eight possible moves it can make, as illustrated below. Each move is two cells in a cardinal direction, then one cell in an orthogonal direction.
    
    
    Each time the knight is to move, it chooses one of eight possible moves uniformly at random (even if the piece would go off the chessboard) and moves there.
    
    The knight continues moving until it has made exactly k moves or has moved off the chessboard.
    
    Return the probability that the knight remains on the board after it has stopped moving.
    
     
    
    Example 1:
    
    Input: n = 3, k = 2, row = 0, column = 0
    Output: 0.06250
    Explanation: There are two moves (to (1,2), (2,1)) that will keep the knight on the board.
    From each of those positions, there are also two moves that will keep the knight on the board.
    The total probability the knight stays on the board is 0.0625.
    Example 2:
    
    Input: n = 1, k = 0, row = 0, column = 0
    Output: 1.00000
     
    
    Constraints:
    
    1 <= n <= 25
    0 <= k <= 100
    0 <= row, column <= n - 1

---
+++ 688. Knight Probability in Chessboard.cpp
@@ -0,0 +1,37 @@
+Output Status :
+
+Runtime
+3 ms
+Beats
+99.65%
+Memory
+6.7 MB
+Beats
+81.9%
+
+class Solution {
+public:
+double dp[26][26][101];
+    double knight(int n , int k, int row, int column){
+         //base cases
+          if(row <0 || column <0|| row>= n ||column >= n){
+             return 0;
+             }
+          if(k ==0) return 1; // if k ie. no. of steps is 0 than the probabilty to have Knight on borad is 1
+          if(dp[row][column][k]){
+              return dp[row][column][k];
+         }
+          double sum =0; // to find the sum
+        
+         sum = knight(n, k-1, row-2, column -1) + knight(n, k-1, row-1, column -2) + 
+            knight(n, k-1, row+2, column +1) + knight(n, k-1, row+1, column +2)+ 
+            knight(n, k-1, row-2, column +1)+ knight(n, k-1, row+2, column -1) +
+            knight(n, k-1, row-1, column +2) + knight(n, k-1, row+1, column -2);
+          sum /= 8;
+          return dp[row][column][k] = sum;
+                                                            
+    }
+    double knightProbability(int n, int k, int row, int column) {
+        return knight(n, k, row, column);
+    }
+};
