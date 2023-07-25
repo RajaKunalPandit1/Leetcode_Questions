d700fb914c1ab675339ff9265eff1f168d5f9951
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2023-07-25 22:26:39 +0530

    Added solution - LeetHub

--- Level order traversal in spiral form - GFG/level-order-traversal-in-spiral-form.cpp
+++ Level order traversal in spiral form - GFG/level-order-traversal-in-spiral-form.cpp
@@ -111,62 +111,78 @@ int main() {
 // } Driver Code Ends
 
 
-/* A binary tree node has data, pointer to left child
-   and a pointer to right child  
-struct Node
-{
-    int data;
-    struct Node* left;
-    struct Node* right;
-    
-    Node(int x){
-        data = x;
-        left = right = NULL;
-    }
-}; */
+// Function to find height of the binary tree
 
+
+//Function to find the height of the tree.
+int height(struct Node *root)
+{
+    //base case for recursion.
+    if(root==NULL)
+        return 0;
+
+    //finding height of left subtree recursively.
+    int l = 1+height(root->left);
+
+    //finding height of right subtree recursively.
+    int r=1+height(root->right);
+    
+    //taking the maximum as height of tree.
+    if(l>r)
+        return l;
+    else
+        return r;
+
+}
+
+//Function to do level order traversal of tree in spiral form.
+void spiral(vector<int> &vec, struct Node *root,int level ,int flag)
+{
+    //base case for recursion.
+    if(root==NULL)
+        return;
+
+    //if level is 1, we push the data in the list.
+    if(level==1)
+        vec.push_back(root->data);
+
+    else if(level>1)
+    {
+        //if flag is true, we call the spiral function recursively for the
+        //left subtrees first and then for right subtrees.
+        if(flag)
+        {
+            spiral(vec,root->left,level-1,flag);
+            spiral(vec,root->right,level-1,flag);
+        }
+        //else we call for right subtrees first and then left subtrees.
+        else
+        {
+            spiral(vec,root->right,level-1,flag);
+            spiral(vec, root->left,level-1,flag);
+        }
+    }
+}
 
 //Function to return a list containing the level order traversal in spiral form.
-vector<int> findSpiral(Node *root)
+vector<int> findSpiral(struct Node *root)
 {
-    vector<int> res;
-    
-    if(root == NULL) return res;
-    
-    queue<Node *> q;
-    q.push(root);
-    
-    bool flag = true;
-    
-    while(!q.empty()){
-        
-        int n = q.size();
-        
-        vector<int> temp;
-        
-        for(int i=0;i<n;i++){
-            
-            Node *curr = q.front();
-            q.pop();
-            
-            temp.push_back(curr->data);
-            
-            if(curr->left != NULL){
-                q.push(curr->left);
-            }
-            
-            if(curr->right != NULL){
-                q.push(curr->right);
-            }
-        }
-        
-        if(!flag){
-            res.insert(res.end(),temp.begin(),temp.end());
-        }else{
-            reverse(temp.begin(),temp.end());
-            res.insert(res.end(),temp.begin(),temp.end());
-        }
-        flag = !flag;
+    if(root==NULL){
+        vector<int> vec;
+        return vec;
     }
-    return res;
+
+    int h = height(root);
+    bool flag = false;
+
+    //for each level, we do the spiral traversal.
+    vector<int> vec;
+    for(int i=1;i<=h;i++)
+    {
+        spiral(vec,root,i,flag);
+        flag=!flag;
+    }
+    
+    //returning the list.
+    return vec;
 }
