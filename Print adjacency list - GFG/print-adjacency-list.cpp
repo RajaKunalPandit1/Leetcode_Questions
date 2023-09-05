27a8f8184f92e63bb783ad8389dc3b6e6cc0a8c8
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2023-09-05 19:18:48 +0530

    Added solution - LeetHub

---
+++ Print adjacency list - GFG/print-adjacency-list.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to return the adjacency list for each vertex.
+    vector<vector<int>> printGraph(int V, vector<pair<int,int>>edges) {
+        vector<int> adj[V];
+        
+        for(int i=0;i<edges.size();i++){
+            int u=edges[i].first;
+            int v=edges[i].second;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        
+        vector<vector<int>> adjList;
+        
+        for(int i=0;i<V;i++){
+             vector<int>temp;
+            for(int j=0;j<adj[i].size();j++){
+                 temp.push_back(adj[i][j]);
+            }
+            sort(temp.begin(),temp.end());
+            adjList.push_back(temp);
+        }
+        
+        return adjList;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+        vector<pair<int,int>>edges;
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            edges.push_back({u,v});
+        }
+        Solution obj;
+        vector<vector<int>> adj = obj.printGraph(V, edges);
+        for(int i=0;i<V;i++)
+        {
+            sort(adj[i].begin(),adj[i].end());
+            for(auto it:adj[i])
+                cout<<it<<" ";
+            cout<<endl;
+        }
+    }
+    return 0;
+}
+// } Driver Code Ends
