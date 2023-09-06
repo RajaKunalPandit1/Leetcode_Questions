0b2e15843ae5dfeef4106f2a6d2f6ec828c93895
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2023-09-06 12:09:53 +0530

    Create 725. Split Linked List in Parts.cpp
    Given the head of a singly linked list and an integer k, split the linked list into k consecutive linked list parts.
    
    The length of each part should be as equal as possible: no two parts should have a size differing by more than one. This may lead to some parts being null.
    
    The parts should be in the order of occurrence in the input list, and parts occurring earlier should always have a size greater than or equal to parts occurring later.
    
    Return an array of the k parts.
    
     
    
    Example 1:
    
    
    Input: head = [1,2,3], k = 5
    Output: [[1],[2],[3],[],[]]
    Explanation:
    The first element output[0] has output[0].val = 1, output[0].next = null.
    The last element output[4] is null, but its string representation as a ListNode is [].
    Example 2:
    
    
    Input: head = [1,2,3,4,5,6,7,8,9,10], k = 3
    Output: [[1,2,3,4],[5,6,7],[8,9,10]]
    Explanation:
    The input has been split into consecutive parts with size difference at most 1, and earlier parts are a larger size than the later parts.
     
    
    Constraints:
    
    The number of nodes in the list is in the range [0, 1000].
    0 <= Node.val <= 1000
    1 <= k <= 50

---
+++ 725. Split Linked List in Parts.cpp
@@ -0,0 +1,40 @@
+Output Status : 
+
+Runtime
+10 ms
+Beats
+11.63%
+Memory
+9 MB
+Beats
+15.59%
+
+class Solution {
+public:
+    int countNodes(ListNode* head) {
+        int n = 0;
+        while (head) {
+            n++;
+            head = head->next;
+        }
+        return n;
+    }
+    
+    vector<ListNode*> splitListToParts(ListNode* head, int k) {
+        int n = countNodes(head);
+        int part_size = n / k, left = n % k;
+        ListNode *ptr = head, *prev = NULL;
+        
+        vector<ListNode*> res(k);
+        for (int i = 0; i < k; i++) {
+            res[i] = head;
+            
+            for (int j = 0; j < part_size + (i < left); j++) {
+                prev = head; 
+                head = head->next; 
+            }
+            if (prev) prev->next = NULL;
+        }
+        return res;
+    }
+};
