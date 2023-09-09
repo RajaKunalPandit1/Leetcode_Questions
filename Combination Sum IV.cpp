16c6d2862c1584adbab5b38bf6f6a75eeb35e374
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2023-09-09 11:38:48 +0530

    Create Combination Sum IV.cpp
    Given an array of distinct integers nums and a target integer target, return the number of possible combinations that add up to target.
    
    The test cases are generated so that the answer can fit in a 32-bit integer.
    
     
    
    Example 1:
    
    Input: nums = [1,2,3], target = 4
    Output: 7
    Explanation:
    The possible combination ways are:
    (1, 1, 1, 1)
    (1, 1, 2)
    (1, 2, 1)
    (1, 3)
    (2, 1, 1)
    (2, 2)
    (3, 1)
    Note that different sequences are counted as different combinations.
    Example 2:
    
    Input: nums = [9], target = 3
    Output: 0
     
    
    Constraints:
    
    1 <= nums.length <= 200
    1 <= nums[i] <= 1000
    All the elements of nums are unique.
    1 <= target <= 1000
     
    
    Follow up: What if negative numbers are allowed in the given array? How does it change the problem? What limitation we need to add to the question to allow negative numbers?

---
+++ Combination Sum IV.cpp
@@ -0,0 +1,36 @@
+Output Status : 
+
+Runtime
+0 ms
+Beats
+100%
+Memory
+6.7 MB
+Beats
+46.24%
+
+class Solution {
+public:
+    
+    int cs(vector<int> &nums, int target, vector<int> &dp){
+        
+        if(dp[target] > -1) return dp[target];
+        int count = 0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]<=target){
+                count += cs(nums,target-nums[i],dp);
+            }
+        }
+        dp[target] = count;
+        return count;
+        
+    }
+    
+    int combinationSum4(vector<int>& nums, int target) {
+        
+        vector<int> dp(target+1,-1);
+        dp[0] = 1;
+        cs(nums,target,dp);
+        return dp[target];
+    }
+};
